c646aea3f94550df5400504fbc0890fafb0d06f9
Duke <dukekhaos@gmail.com>
2014-12-13 15:41:12 -0200

    starting PT translate

---
+++ lang/pt.js
@@ -0,0 +1,21 @@
+export default {
+  // COMPONENTS
+    // EPISODE-LIST-ITEM
+    'components.episode_list_item.play': "Tocar",
+    'components.episode_list_item.pause': "Pausar",
+    'components.episode_list_item.info': "Info",
+
+    // SUBSCRIBE-BUTTON
+    'components.subscribe_button.subscribe': 'Assinar!',
+
+  // SIDEBAR
+  'sidebar.channel_add': 'Buscar Canais',
+  'sidebar.donate': 'Faça uma doação',
+  'sidebar.home': 'Inicio',
+  'sidebar.settings': 'Configurações',
+  'sidebar.sign_out': 'Sair',
+
+  // TITLES
+  'titles.episodes': 'Episódios'
+};
+
